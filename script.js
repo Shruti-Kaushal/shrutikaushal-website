34a50148ccb2deca866ff6305afcfe727e39c033
Shruti-Kaushal <shrutikaushal95@gmail.com>
2026-02-04 16:37:04 -0500

    updating all files

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// Tiny helper: set current year in footer
-document.getElementById("year").textContent = new Date().getFullYear();
+const btn = document.querySelector("#theme-toggle");
+const currentTheme = localStorage.getItem("theme");
 
+if (currentTheme === "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
