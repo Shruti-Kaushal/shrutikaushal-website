a8bf7ec4b8ac8ccf9a777dc56ed088870ba983d8
Shruti-Kaushal <shrutikaushal95@gmail.com>
2026-02-06 16:49:54 -0500

    basic animation

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const btn = document.querySelector("#theme-toggle");
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme === "dark") {
-  document.body.classList.add("dark-theme");
+    document.body.setAttribute("data-theme", "dark");
 }
 
-btn.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
-  let theme = "light";
-  if (document.body.classList.contains("dark-theme")) {
-    theme = "dark";
-  }
-  localStorage.setItem("theme", theme);
+btn.addEventListener("click", () => {
+    let theme = document.body.getAttribute("data-theme");
+    if (theme === "dark") {
+        document.body.removeAttribute("data-theme");
+        localStorage.setItem("theme", "light");
+    } else {
+        document.body.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
 });
