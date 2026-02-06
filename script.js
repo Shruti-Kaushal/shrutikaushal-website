6dddf1118f0b607f6ca7c9d1901ed21222ff114f
Shruti-Kaushal <shrutikaushal95@gmail.com>
2026-02-06 18:18:04 -0500

    fixed theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// --- THEME TOGGLE LOGIC ---
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    const root = document.documentElement;
+    // Check current theme and toggle it
+    const currentTheme = root.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    root.setAttribute('data-theme', newTheme);
+});
 const nodes = document.querySelectorAll('.step-node');
 const tooltip = document.getElementById('experience-tooltip');
 
